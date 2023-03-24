9573e47344556241d6bf9253d315d5e93550dd71
Danyal Chatha <114454211+DanyalChatha@users.noreply.github.com>
2023-03-24 10:07:17 -0400

    Update my-module.js

--- public/my-module.js
+++ public/my-module.js
@@ -1,4 +1,19 @@
-
 import { theShire, buckland} from "./another-module";
 
 export {theShire, statement};
+
+import { expect } from 'chai';
+
+describe('buckland', function() {
+	it('should be Hobbit Settlement', () => {
+		expect(buckland()).to.be.an.instanceof(HobbitSettlement);
+	});
+});
+
+
+describe('statement', function() {
+	it('Should return the correct string', () => {
+		expect(statement()).to.equal('The Shire was founded 739 years before Buckland.');
+	});
+});
+
